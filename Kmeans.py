ca55c6e375afbe0c503b524fd54e68396b06e184
Lucas de Andrade Lima <spirolucas@hotmail.com>
2018-09-22 09:53:02 -0300

    Versão 2.0
    Adição de uma classe e um método, finalização do método Kmeans.

--- Kmeans.py
+++ Kmeans.py
@@ -1,10 +1,33 @@
 #Versão inicial do kmeans feito por mim
+class Cluster(object):
+
+    def __init__(self, ponto):
+        self.lista_pontos = [].append(ponto)
+        self.centroide = ponto
+
+    def alt_centroide(self, n_ponto):
+        x = (self.centroide[0] + n_ponto[0])/2
+        y = (self.centroide[1] + n_ponto[1])/2
+        self.centroide = [x, y]
 
 
-#função receberá duas listas contendo valores representativos de x e y e retorna a centroide entre eles
-def ret_centroide(ponto_a, ponto_b):
-    centroide = [(ponto_a[0]+ponto_b[0])/2, (ponto_a[1]+ponto_b[1])/2]
-    return centroide
+def checagem_dist(ponto, lista_pontos):
+    aux_index = None
+    aux_eud = 0
+    for i in range(len(lista_pontos)):
+        if aux_eud == 0:
+            aux_eud = dist_euclid(ponto, lista_pontos[i])
+            aux_ponto = lista_pontos[i]
+            aux_index = i
+
+        elif dist_euclid(ponto, lista_pontos[i]) < aux_eud:
+            aux_eud = dist_euclid(ponto, lista_pontos[i])
+            aux_ponto = lista_pontos[i]
+            aux_index = i
+
+
+    return aux_index, aux_ponto
+
 
 #função receberá duas listas contendo valores representativos de x e y e retorna a distância euclidiana entre eles
 def dist_euclid(ponto_a, ponto_b):
@@ -23,7 +46,13 @@ def Kmeans(n_grupos, lista_pontos):
     #internas, representando os clusters.
     lista_organizada = []
     for aux in range(n_grupos):
-        lista_organizada.append([])
-        #Obtenção dos primeiros pontos e remoção destes na lista geral.
-        lista_organizada[aux].append(lista_pontos.pop(indices_r[aux]))
+        # Obtenção dos primeiros pontos e remoção destes na lista geral.
+        lista_organizada.append(Cluster(lista_pontos.pop(indices_r[aux])))
 
+    while len(lista_pontos) != 0:
+        for cluster in lista_organizada:
+            index, pont = checagem_dist(cluster.centroide, lista_pontos)
+            cluster.lista_pontos.append(lista_pontos.pop(index))
+            cluster.alt_centroide(pont)
+
+    return lista_organizada
